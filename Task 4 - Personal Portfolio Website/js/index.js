f8aa49376460f05bcf86a6ecc9c83c74f11a9147
unknown <rawalsaumya22@gmail.com>
2024-06-26 18:39:01 +0530

    Task 4 done

--- Task 4 - Personal Portfolio Website/js/index.js
+++ Task 4 - Personal Portfolio Website/js/index.js
@@ -15,3 +15,13 @@ function opentab(tabname){
 
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+let sidemenu=document.getElementById('sidemenu');
+
+function openmenu(){
+    sidemenu.style.right='0';
+}
+
+function closemenu(){
+    sidemenu.style.right='-200px';
+}
